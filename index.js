071683e84211616613488a1bfa11085876a135f6
Mechboy1 <olamoshood9436.mi@gmail.com>
2022-11-10 19:33:51 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const menuEL = document.querySelector(".menuContainer");
+
+const openMenu = () ==> {
+    menuEL.classList.add("show"); 
+};
+const closeMenu = () ==> {
+    menuEL.classList.remove("show");
+};
